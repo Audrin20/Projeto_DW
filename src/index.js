3cc8a4df4b48e51a793f3c4e0b8bbd1f83c6b27b
Audrin20 <audrinh@gmail.com>
2021-04-15 20:44:44 -0300

    testando

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send('Hello World!');
+    res.send('');
 });
+
+app.get('/hello/pt',(req, res) => {
+    res.send('Olá, Mundo!')
+})
 
 app.listen(3000, console.log('servidor rodando na porta: 3000'));
